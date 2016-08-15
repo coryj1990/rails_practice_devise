d9aa94f665dc21430e36d3aeaa51a87165d12fa2
Cory Johnson <cory.r.johnson@cox.net>
2016-08-15 12:42:53 -0500

    pushing to merge again

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'mustauth/index' => 'mustauth#index'
   get 'noauth/index' => 'noauth#index'
 
-  devise_for :users
+ devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
